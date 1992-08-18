41cec12254f6149493826791a56619fadd300cf0
Guido van Rossum <guido@python.org>
1992-08-18 14:16:12 +0000

    Initial revision

--- Demo/sgi/video/vinfo.py
+++ Demo/sgi/video/vinfo.py
@@ -1,90 +1,38 @@
-from gl import *
-from GL import *
-from DEVICE import *
-import time
 import sys
-import getopt
-
-class Struct(): pass
-epoch = Struct()
-EndOfFile = 'End of file'
-bye = 'bye'
-
-def openvideo(filename):
-	f = open(filename, 'r')
-	line = f.readline()
-	if not line: raise EndOfFile
-	if line[:4] == 'CMIF': line = f.readline()
-	x = eval(line[:-1])
-	if len(x) == 3: w, h, pf = x
-	else: w, h = x; pf = 2
-	return f, w, h, pf
-
-def loadframe(f, w, h, pf):
-	line = f.readline()
-	if line == '':
-		raise EndOfFile
-	x = eval(line[:-1])
-	if type(x) == type(0) or type(x) == type(0.0):
-		tijd = x
-		if pf == 0:
-			size = w*h*4
-		else:
-			size = (w/pf) * (h/pf)
-	else:
-		tijd, size = x
-	f.seek(size, 1)
-	return tijd
+import VFile
 
 def main():
-	delta = 0
-	short = 0
-	try:
-		opts, names = getopt.getopt(sys.argv[1:], 'ds')
-	except getopt.error, msg:
-		sys.stderr.write(msg + '\n')
-		sys.stderr.write('usage: vinfo [-d] [-s] [file] ...\n')
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-d': delta = 1	# print delta between frames
-		elif opt == '-s': short = 1	# short: don't print times
-	if names == []:
-		names = ['film.video']
-	for name in names:
-	    try:
-		f, w, h, pf = openvideo(name)
-	    except:
-	    	sys.stderr.write(name + ': cannot open\n')
-	    	continue
-	    if pf == 0:
-	        size = w*h*4
-	    else:
-	        size = (w/pf) * (h/pf)
-	    print name, ':', w, 'x', h, '; pf =', pf, ', size =', size,
-	    if pf == 0:
-		    print '(color)',
-	    else:
-		    print '(' + `(w/pf)` + 'x' + `(h/pf)` + ')',
-		    if (w/pf)%4 <> 0: print '!!!',
-	    print
-	    num = 0
-	    try:
-	    	otijd = 0
-		while not short:
-		    try:
-			tijd = loadframe(f, w, h, pf)
-			if delta: print '\t' + `tijd-otijd`,
-			else: print '\t' + `tijd`,
-			otijd = tijd
-			num = num + 1
-			if num % 8 == 0:
-				print
-		    except EndOfFile:
-			raise bye
-	    except bye:
-		pass
-	    if num % 8 <> 0:
-		print
-	    f.close()
+	if sys.argv[1:]:
+		for filename in sys.argv[1:]:
+			process(filename)
+	else:
+		process('film.video')
+
+def process(filename):
+	vin = VFile.VinFile().init(filename)
+	print 'File:    ', filename
+	print 'Version: ', vin.version
+	print 'Size:    ', vin.width, 'x', vin.height
+	print 'Pack:    ', vin.packfactor, '; chrom:', vin.chrompack
+	print 'Bits:    ', vin.c0bits, vin.c1bits, vin.c2bits
+	print 'Format:  ', vin.format
+	print 'Offset:  ', vin.offset
+	print 'Frame times:',
+	n = 0
+	t = 0
+	while 1:
+		try:
+			t, data, cdata = vin.getnextframe()
+		except EOFError:
+			print
+			break
+		if n%8 == 0:
+			sys.stdout.write('\n')
+		sys.stdout.write('\t' + `t`)
+		n = n+1
+	print 'Total', n, 'frames in', t*0.001, 'sec.',
+	if t:
+		print '-- average', int(n*10000.0/t)*0.1, 'frames/sec',
+	print
 
 main()
