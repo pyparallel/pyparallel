c1e5947811a8c859afc50716dff320247f8b0238
Trent Nelson <trent@trent.me>
2014-08-16 00:31:57 -0400

    Enable WITH_PYMALLOC again.

--- PC/pyconfig.h
+++ PC/pyconfig.h
@@ -575,11 +575,7 @@ Py_NO_ENABLE_SHARED to find out.  Also support MS_NO_COREDLL for b/w compat */
 /* #define WITH_READLINE 1 */
 
 /* Use Python's own small-block memory-allocator. */
-#ifdef Py_DEBUG
 #define WITH_PYMALLOC 1
-#else
-#undef WITH_PYMALLOC
-#endif
 
 /* Define if you have clock.  */
 /* #define HAVE_CLOCK */
