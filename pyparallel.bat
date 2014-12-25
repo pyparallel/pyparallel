9452382ab68b12c2f81eb2d9de7acf6ab454eb20
Trent Nelson <trent@trent.me>
2014-12-24 22:31:40 -0500

    Add pyparallel.bat and pyparallel_pgo.bat helpers.

---
+++ pyparallel.bat
@@ -0,0 +1,4 @@
+@echo off
+set PATH=%~dp0\PCBuild\amd64:%PATH%
+set PYTHONPATH=%~dp0\Lib
+PCbuild\amd64\python
---
+++ pyparallel_pgo.bat
@@ -0,0 +1,4 @@
+@echo off
+set PATH=%~dp0\PCBuild\x64-pgo:%PATH%
+set PYTHONPATH=%~dp0\Lib
+PCbuild\x64-pgo\python.exe
