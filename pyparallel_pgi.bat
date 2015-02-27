28ba013519c633fc1a4efcd6f321adcb9fe3fdd5
Trent Nelson <trent@trent.me>
2015-02-27 16:00:31 -0500

    Fix the pgi/pgo .bat files.
    (Note that python.exe is still bombing out as soon as you try and run an instrumented binary.
     Haven't investigated yet.)

--- pyparallel_pgi.bat
+++ pyparallel_pgi.bat
@@ -1,5 +1,5 @@
 @echo off
-set PATH=%~dp0\PCBuild\x64-pgo:%PATH%
+set PATH=%~dp0\PCBuild\x64-pgi:%PATH%
 set PYTHONPATH=%~dp0\Lib
 call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
-PCbuild\x64-pgo\python.exe %*
+PCbuild\x64-pgi\python.exe %*
