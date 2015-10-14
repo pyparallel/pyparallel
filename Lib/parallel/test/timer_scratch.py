8697a3da29022d8c98b7f063959c7f01872d513e
Trent Nelson <trent@trent.me>
2015-10-14 18:38:01 -0400

    Update timer examples.

--- Lib/parallel/test/timer_scratch.py
+++ Lib/parallel/test/timer_scratch.py
@@ -19,13 +19,46 @@ from parallel.http.server import (
     HttpServer,
 )
 
-def update_stats():
+def update_sys_stats():
+    return ujson.dumps(sys_stats())
+
+def update_stats(t):
     return ujson.dumps({
+        'count': t.count,
         'timestamp': gmtime(),
-        'system': dict(sys_stats()),
         'memory': dict(memory_stats()),
         'contexts': dict(context_stats()),
         'thread': thread_seq_id(),
     }).encode('utf-8')
 
-t = timer(datetime.timedelta(seconds=2), 50, update_stats)
+t = timer(datetime.timedelta(milliseconds=1), 1000, update_stats)
+t.args = (t,)
+t.start()
+
+class RateLimitedServer(HttpServer):
+    http11 = True
+    rate_limit = datetime.timedelta(milliseconds=16)
+    def hello(self, transport, data):
+        return b'Hello, World!'
+    def stats(self, transport, data):
+        return t.data or b''
+    def uni(self, transport, data):
+        return '<html><body>Works!</body></html>'
+    def bytearr(self, transport, data):
+        return bytearray(b'abcd')
+
+class NormalServer(HttpServer):
+    http11 = True
+    def hello(self, transport, data):
+        return b'Hello, World!'
+    def stats(self, transport, data):
+        return t.data or b''
+    def uni(self, transport, data):
+        return '<html><body>Works!</body></html>'
+    def bytearr(self, transport, data):
+        return bytearray(b'abcd')
+
+server1 = parallel.server('0.0.0.0', 8080)
+server2 = parallel.server('0.0.0.0', 8081)
+parallel.register(server1, RateLimitedServer)
+parallel.register(server2, NormalServer)
