e0589a2f6b6ef3dfc672f7ca1efde92811ec76e1
Trent Nelson <trent@trent.me>
2015-10-22 14:33:12 -0400

    Add vim modeline.

--- contrib/windows/_adv.h
+++ contrib/windows/_adv.h
@@ -8,3 +8,5 @@ typedef struct _OPERATION_PARAMS {
     OPERATION_START_PARAMETERS  start;
     OPERATION_END_PARAMETERS    end;
 } OPERATION_PARAMS, *POPERATION_PARAMS;
+
+/* vim: set ts=8 sw=4 sts=4 tw=80 et:                                         */
