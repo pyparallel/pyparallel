3835e3911050133cb56731ae5c80261954fcb108
Trent Nelson <trent@trent.me>
2015-11-05 12:03:35 -0500

    Fix bundled Cython packaging.

---
+++ Scripts/cythonize-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\Trent\Home\src\pyparallel\PCbuild\amd64\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Cython==0.23.4','console_scripts','cythonize'
+__requires__ = 'Cython==0.23.4'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Cython==0.23.4', 'console_scripts', 'cythonize')()
+    )
